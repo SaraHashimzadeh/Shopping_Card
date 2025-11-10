5e08a5f4a678fd04c494676d252d91d891ff61b4
SaraHAshimzadeh <sara.hashimzade@gmail.com>
2025-11-10 16:53:20 +0300

    card open close

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //Cart OPen Close
+let cartIcon = document.querySelector(".cart-icon");
+let cartModel = document.querySelector(".cart-model");
+let closeBtn = document.querySelector(".close-btn");
 
+cartIcon.addEventListener("click", function(){
+    cartModel.classList.add("open-cart");
+});
+
+closeBtn.addEventListener("click", function(){
+    cartModel.classList.remove("open-cart")
+});
