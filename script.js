609a4a5cc22fe2a4b92276f21ef92e89b361b922
SaraHAshimzadeh <sara.hashimzade@gmail.com>
2025-11-15 16:29:14 +0300

    cart update

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
 //Cart
-let cartItems = JSON.parse(localStorage.getItem('cartItems')) || []; 
+let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 let total = 0;
 let itemCount = 0;
 
 //Add to cart
-function addToCart(productCard){
+function addToCart(productCard) {
 
-const name = productCard.querySelector('.product-name').textContent;
-const priceText = productCard.querySelector(".product-price").textContent;
-const price = parseFloat(priceText.replace('$', ''));
-const imgSrc = productCard.querySelector('.product-image').src;
+    const name = productCard.querySelector('.product-name').textContent;
+    const priceText = productCard.querySelector(".product-price").textContent;
+    const price = parseFloat(priceText.replace('$', ''));
+    const imgSrc = productCard.querySelector('.product-image').src;
 
-const existingItem = cartItems.find((item) => item.name === name);
+    const existingItem = cartItems.find((item) => item.name === name);
 
-if(existingItem) {
-    existingItem.quantity +=1;
-} else {
-    cartItems.push ({
-        name,
-        price,
-        quantity: 1,
-        image:imgSrc
-    });
-}
-updateLocalStorage();
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cartItems.push({
+            name,
+            price,
+            quantity: 1,
+            image: imgSrc
+        });
+    }
+    updateLocalStorage();
+    updateCartDisplay();
 
 }
 //Show cart item in cart
-function updateCartDispla() {
+function updateCartDisplay() {
     const cartList = document.getElementById('cart-items')
     const totalElement = document.getElementById('total-price')
     const countElement = document.getElementById('cart-count');
 
     cartList.innerHTML = '';
-    total = cartItems.reduce((sum, item) => sum +item)
+    total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    itemCount = cartItems.reduce((count, item) => count + item.quantity, 0);
+
+    cartItems.forEach((item) => {
+        const li = document.createElement('li');
+        li.classList = 'cart-item';
+        li.innerHTML = `
+        <img src="${item.image}" alt="${item.name}" class="cart-item-image">
+        <div class="cart-item-details">
+            <div class="cart-item-name">${item.name}</div>
+            <div class="cart-item-price">$${item.price} * ${item.quantity}</div>
+        </div>
+
+        <div class="quantity-controls">
+            <button onclick="changeQuantity('${item.name}', -1)">-</button>
+            <button onclick="changeQuantity('${item.name}', 1)">+</button>
+        </div>
+
+         <button class="remove" onclick="removeItem('('${item.name}')')">x</button>
+        `;
+        cartList.appendChild(li);
+    });
+    totalElement.textContent = total.toFixed(2);
+    countElement.textContent = itemCount;
 }
 
 
@@ -48,10 +72,10 @@ let cartIcon = document.querySelector(".cart-icon");
 let cartModel = document.querySelector(".cart-model");
 let closeBtn = document.querySelector(".close-btn");
 
-cartIcon.addEventListener("click", function(){
+cartIcon.addEventListener("click", function () {
     cartModel.classList.add("open-cart");
 });
 
-closeBtn.addEventListener("click", function(){
+closeBtn.addEventListener("click", function () {
     cartModel.classList.remove("open-cart")
 });
